d06df04a7fadebd773791e4a30899c383d50d202
nupur-cs <nupur.rathya@gmail.com>
2025-12-10 16:27:01 +0530

    Changes done by Nupur

--- Git-Ex1.js
+++ Git-Ex1.js
@@ -63,3 +63,50 @@ letter grades */
     return 'f';
  }
 }
+
+//Changes done by Nupur
+// Teacher
+const t1 = new Teacher(1, "Mr. Sharma");
+ 
+// Classroom
+const classA = new Classroom("A1", t1);
+ 
+// Demo Students
+const s1 = { id: 101, name: "Amit" };
+const s2 = { id: 102, name: "Riya" };
+const s3 = { id: 103, name: "Karan" };
+ 
+// Add Students
+classA.addStudent(s1);
+classA.addStudent(s2);
+classA.addStudent(s3);
+ 
+// Attendance Demo
+classA.markPresent(101);
+classA.markPresent(101);
+classA.markPresent(102);
+classA.markPresent(103);
+classA.markPresent(103);
+classA.markPresent(103);
+ 
+// Grade Demo
+classA.gradebook.record(101, "Math Test", 90);
+classA.gradebook.record(101, "Science Test", 80);
+ 
+classA.gradebook.record(102, "Math Test", 85);
+classA.gradebook.record(102, "Science Test", 92);
+ 
+classA.gradebook.record(103, "Math Test", 70);
+ 
+// Display Results
+console.log("\n--- AVERAGES ---");
+console.log("Amit:", classA.gradebook.average(101));
+console.log("Riya:", classA.gradebook.average(102));
+console.log("Karan:", classA.gradebook.average(103));
+ 
+console.log("\n--- ATTENDANCE % ---");
+console.log("Amit:", classA.attendancePercentage(101), "%");
+console.log("Riya:", classA.attendancePercentage(102), "%");
+console.log("Karan:", classA.attendancePercentage(103), "%");
+ 
+ 
